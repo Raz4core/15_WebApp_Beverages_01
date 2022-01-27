f89921dec4e4ab5dfd9a89f7c8a3a22b96b97434
Raz4core <mail@test.de>
2022-01-27 09:21:28 +0100

    Modul: Buisness-Logic (Mapping)

--- js/view_controller.js
+++ js/view_controller.js
@@ -5,23 +5,40 @@
     Einlesen Daten von Webseite :: 
 	Check Daten :: 
     Btn. Trigger :: 
-    Business-Logic (Alter --> Getränk) :: 
-    Bild austauschen :: 
+    Business-Logic (Alter --> Getränk) :: Check!
+    Bild austauschen :: Check!
 */
 
 /***  Ablaufsteuerung | Control */  
 
+/**Business-Logic */
+//3.Modul: Buisness-Logic (Mapping)
+// output(checkAge(5));
+
+function checkAge(age) {
+    switch (true) {
+        case (age >= 0) && (age <= 5) :
+            return "milch";           
+        case (age >= 6) && (age <= 12):
+            return "saft";
+        case (age >= 13) && (age <= 17):
+            return "cola";
+        case (age >= 18) && (age <= 130):
+            return "wein";
+        default:
+            return "tee";
+    }
+}
 
 
 /*** View-Schicht aktualisieren */ 
-//1.Modul: Bild aktualisieren I Test:
-output(updateImg("cola"));
-// output(updateImg("milch"));
+//2.Modul: Bild aktualisieren I Test:
+//output(updateImg("cola"));
+//output(updateImg("milch"));
 // output(updateImg("wein"));
 function updateImg(imgName) {
     const img = document.getElementById("bevImg");
-    img.src = "./bilder/cola.jpg";
-    // output(img.src);
+    img.src = "./bilder/" + imgName + ".jpg";
     return imgName;
 }
 
